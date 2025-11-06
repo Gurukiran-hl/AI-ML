3521b9a6465f7ba59e92d844430e475ec6748b87
Gurukiran HL <gurukiranhl07@gmail.com>
2025-11-06 20:40:25 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,18 @@
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
+
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+
+# While with condition
+password = ""
+while password != "gk007":
+    password = input("Enter password: ")
+
+print("Access granted!")
